d8e736fd9657340915c640fabb7eea80de67fe05
Juliana Araujo <julianaaraujo21@gmail.com>
2023-05-08 08:37:08 -0300

    configuração(tsconfig,package)

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,4 +1,3 @@
 import { Negociacao } from './models/negociacao.js';
-
-const negociacao = new Negociacao(new Date(),10,400);
+const negociacao = new Negociacao(new Date(), 10, 400);
 console.log(negociacao.volume);
